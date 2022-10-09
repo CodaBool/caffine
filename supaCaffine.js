4f3ead33baf909889dee6e1a592bac997f4887a6
Coda Bool <codabool@pm.me>
2022-10-09 11:18:17 -0400

    better printing

--- supaCaffine.js
+++ supaCaffine.js
@@ -1,14 +1,14 @@
 require('dotenv').config()
 const pgp = require('pg-promise')();
 
-async function read(connectionString, table) {
+async function read(connectionString, project) {
   const db = pgp(connectionString)
   const res = await db.any(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`)
-  console.log('res', res)
+  console.log(project, 'has', res.length, 'tables' )
 }
 
-read(process.env.PG_URI_GEN, 'trending_github')
-read(process.env.PG_URI_DEK, 'trending_github')
+read(process.env.PG_URI_GEN, 'general')
+read(process.env.PG_URI_DEK, 'deck')
 pgp.end()
 
 // SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'
