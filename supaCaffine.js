0980bac182fbe3042d37128d7a97c316c4742e6a
Coda Bool <codabool@pm.me>
2022-10-21 00:20:08 -0400

    added write to supa caffine

--- supaCaffine.js
+++ supaCaffine.js
@@ -1,14 +1,24 @@
 require('dotenv').config()
 const pgp = require('pg-promise')();
+const dbGen = pgp(process.env.PG_URI_GEN)
+const dbDek = pgp(process.env.PG_URI_DEK)
 
-async function read(connectionString, project) {
-  const db = pgp(connectionString)
+async function read(db, project) {
   const res = await db.any(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`)
   console.log(project, 'has', res.length, 'tables' )
 }
 
-read(process.env.PG_URI_GEN, 'general')
-read(process.env.PG_URI_DEK, 'deck')
-pgp.end()
+async function write(db, project) {
+  let date = new Date()
+  date = date.toUTCString().slice(0, 25)
+  console.log('setting all rows of table test in project', project, 'to', date)
+  const res = await db.any(`UPDATE test SET caffine='${date}';`)
+}
 
-// SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'
+read(dbGen, 'general')
+read(dbDek, 'deck')
+
+write(dbGen, 'general')
+write(dbDek, 'deck')
+
+pgp.end()
