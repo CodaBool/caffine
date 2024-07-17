8c4ff8dde9a22393f672ab639ff5a3d877afaa7e
codabool <codabool@pm.me>
2024-07-17 14:08:45 -0400

    add new project

--- supaCaffine.js
+++ supaCaffine.js
@@ -1,24 +1,28 @@
 require('dotenv').config()
 const pgp = require('pg-promise')();
 const dbGen = pgp(process.env.PG_URI_GEN)
+const dbMap = pgp(process.env.PG_URI_MAP)
 //const dbDek = pgp(process.env.PG_URI_DEK)
 
 async function read(db, project) {
   const res = await db.any(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`)
-  console.log(project, 'has', res.length, 'tables' )
+  console.log(project, 'said in a very jittery voice that it has', res.length, "tables and it's very tired" )
 }
 
 async function write(db, project) {
   let date = new Date()
   date = date.toUTCString().slice(0, 25)
-  console.log('setting all rows of table test in project', project, 'to', date)
-  const res = await db.any(`UPDATE test SET caffine='${date}';`)
+  console.log('filling coffee for', project)
+  const res = await db.any(`UPDATE caffine SET last_caffine=(to_timestamp(${Date.now()} / 1000.0));`)
+//  const res = await db.any(`UPDATE caffine SET caffine='${date}';`  `UPDATE caffine SET caffine='(to_timestamp(${Date.now()} / 1000.0))'`)
 }
 
 read(dbGen, 'general')
+read(dbMap, 'community-map')
 //read(dbDek, 'deck')
 
 write(dbGen, 'general')
+write(dbMap, 'community-map')
 //write(dbDek, 'deck')
 
 pgp.end()
