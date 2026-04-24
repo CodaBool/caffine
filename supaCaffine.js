281fc4085fdf7c05146715189097d5fbf0913995
CodaBool <CodaBool@pm.me>
2026-04-23 21:09:51 -0400

    code change and bump

--- supaCaffine.js
+++ supaCaffine.js
@@ -1,27 +1,50 @@
-const pgp = require('pg-promise')();
-const dbGen = pgp(process.env.PG_URI_GEN)
-const dbMap = pgp(process.env.PG_URI_MAP)
-//const dbDek = pgp(process.env.PG_URI_DEK)
+const pgp = require("pg-promise")();
+
+const dbGen = pgp(process.env.PG_URI_GEN);
+const dbMap = pgp({
+  connectionString: process.env.PG_URI_GEN,
+  ssl: { rejectUnauthorized: false }
+});
+
 
 async function read(db, project) {
-  const res = await db.any(`SELECT table_name FROM information_schema.tables WHERE table_schema = 'public'`)
-  console.log(project, 'said in a very jittery voice that it has', res.length, "tables and it's very tired" )
+  const res = await db.any(`
+    SELECT table_name
+    FROM information_schema.tables
+    WHERE table_schema = 'public'
+  `);
+
+  console.log(
+    project,
+    "said in a very jittery voice that it has",
+    res.length,
+    "tables and it's very tired"
+  );
 }
 
 async function write(db, project) {
-  let date = new Date()
-  date = date.toUTCString().slice(0, 25)
-  console.log('filling coffee for', project)
-  const res = await db.any(`UPDATE caffine SET last_caffine=(to_timestamp(${Date.now()} / 1000.0));`)
-//  const res = await db.any(`UPDATE caffine SET caffine='${date}';`  `UPDATE caffine SET caffine='(to_timestamp(${Date.now()} / 1000.0))'`)
+  console.log("filling coffee for", project);
+
+  await db.none(`
+    UPDATE caffine
+    SET last_caffine = to_timestamp($1 / 1000.0)
+  `, [Date.now()]);
 }
 
-read(dbGen, 'general')
-read(dbMap, 'community-map')
-//read(dbDek, 'deck')
+async function main() {
+  try {
+    await Promise.all([
+      read(dbGen, "general"),
+      read(dbMap, "community-map"),
+      write(dbGen, "general"),
+      write(dbMap, "community-map"),
+    ]);
+  } finally {
+    pgp.end();
+  }
+}
 
-write(dbGen, 'general')
-write(dbMap, 'community-map')
-//write(dbDek, 'deck')
-
-pgp.end()
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
