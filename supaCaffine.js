f66ea42b4bbe4e7fa7bfe52025ea2da5fb0364b2
CodaBool <codabool@pm.me>
2026-05-27 15:46:30 -0400

    add virtual cyc

--- supaCaffine.js
+++ supaCaffine.js
@@ -1,50 +1,58 @@
-const pgp = require("pg-promise")();
+const pgp = require("pg-promise")()
 
-const dbGen = pgp(process.env.PG_URI_GEN);
+const dbGen = pgp(process.env.PG_URI_GEN)
 const dbMap = pgp({
   connectionString: process.env.PG_URI_GEN,
-  ssl: { rejectUnauthorized: false }
-});
-
+  ssl: { rejectUnauthorized: false },
+})
+const dbBrandon = pgp({
+  connectionString: process.env.PG_URI_BRANDON,
+  ssl: { rejectUnauthorized: false },
+})
 
 async function read(db, project) {
   const res = await db.any(`
     SELECT table_name
     FROM information_schema.tables
     WHERE table_schema = 'public'
-  `);
+  `)
 
   console.log(
     project,
     "said in a very jittery voice that it has",
     res.length,
-    "tables and it's very tired"
-  );
+    "tables and it's very tired",
+  )
 }
 
 async function write(db, project) {
-  console.log("filling coffee for", project);
+  console.log("filling coffee for", project)
 
-  await db.none(`
+  await db.none(
+    `
     UPDATE caffine
     SET last_caffine = to_timestamp($1 / 1000.0)
-  `, [Date.now()]);
+  `,
+    [Date.now()],
+  )
 }
 
 async function main() {
   try {
     await Promise.all([
       read(dbGen, "general"),
-      read(dbMap, "community-map"),
+      read(dbMap, "community map"),
+      read(dbBrandon, "virtual cyclinder"),
       write(dbGen, "general"),
-      write(dbMap, "community-map"),
-    ]);
+      write(dbMap, "community map"),
+      write(dbBrandon, "virtual cyclinder"),
+    ])
   } finally {
-    pgp.end();
+    pgp.end()
   }
 }
 
-main().catch((err) => {
-  console.error(err);
-  process.exit(1);
-});
+main().catch(err => {
+  console.error(err)
+  process.exit(1)
+})
